b57590a317e35d6bda73b8ea7a9cfa90a529e4f7
Kirtika Ruchandani <kirtikar@qti.qualcomm.com>
2014-02-23 19:17:47 -0800

    Saving one cycle by keeping allOnes constant

--- BitHelper.hpp
+++ BitHelper.hpp
@@ -4,6 +4,8 @@
 #include <cassert> 
 
 using namespace std;
+const int32_t allOnes = ~0;
+
 
 inline bool getBit(int32_t num,int32_t i)
 {
@@ -88,7 +90,6 @@ int32_t getBits(int32_t num, int8_t leftIdx, int8_t rightIdx)
 {
   // cout << (int)leftIdx << "-" << (int)rightIdx << endl;
   // Prepare a mask to get only required bits
-  int32_t allOnes = ~0;
   int32_t left;
   if (leftIdx == 31)
     left  = 0;
@@ -104,26 +105,17 @@ int32_t getBits(int32_t num, int8_t leftIdx, int8_t rightIdx)
 // Bits 31....0 => Left..... Right (Big Endian Assumption)
 int32_t updateBits(int32_t numToBeMerged,int32_t numToMergeInto, int8_t leftIdx, int8_t rightIdx)
 {
-  // cout << "UpdateBits called" << endl;
-  // cout << "Merge this "; printBitWise(numToBeMerged);
-  // cout << "into this "; printBitWise(numToMergeInto);
   // Clear bits i through leftIdx in numToMergeInto
-  int32_t allOnes = ~0;
   int32_t left;
   if (leftIdx == 31)
     left = 0;
   else 
     left  = allOnes << (leftIdx+1);
-  // cout << "Left: "; printBitWise(left);
   int32_t right = ((1<<rightIdx) -1);
   int32_t mask =left | right;
-  // cout << "Right: "; printBitWise(right);
-  // cout << "MASK " ; printBitWise(mask);
   int32_t n_cleared = numToMergeInto & mask;
-  // cout << "NCLEARED " ; printBitWise(n_cleared);
   int32_t m_shifted = numToBeMerged << rightIdx;
   int32_t res =(n_cleared | m_shifted);
-  // cout << "UpdateBits result : "; printBitWise(res);
   return res;
 }
 
